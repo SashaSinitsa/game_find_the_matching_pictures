277066d614c3958c0e59cb8e32bfe25380f6ae7d
sasha <t0996602789@gmail.com>
2016-11-19 20:15:35 +0200

    update

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 (function() {
 // сделать AJAX
-    var width = 5;
-        height = 6;
+    var width = 4,
+        height = 5;
 
     function Game() {
+        this.items = width * height;
         this.img = [ 'https://kde.link/test/1.png',
                      'https://kde.link/test/2.png',
                      'https://kde.link/test/9.png',
@@ -14,7 +15,6 @@
                      'https://kde.link/test/0.png',
                      'https://kde.link/test/5.png',
                      'https://kde.link/test/8.png' ];
-
     }
 
     // Game.prototype.pause = pause;
@@ -26,6 +26,7 @@
     Game.prototype.init = function () {
 
         var inner = document.getElementById('click-game');
+
         inner.style.width = this.w = inner.dataset.width;
         inner.style.height = this.h = inner.dataset.height;
 
@@ -49,6 +50,114 @@
 
 
 
+    Game.prototype.start = function () {
+
+        var box, table, newArr;
+
+        this.score = 1000;
+        this.timer = 0;
+        this.center.innerHTML = 
+            "<div class='center-inner'><div class='box' id='box'></div></div>";
+
+        // create the array of pairs of pictures
+        newArr = createArr(this.img, this.items); 
+        box = document.getElementById('box');
+
+        // creates the table of pictures
+        table = createTable.call(this, newArr);
+        box.appendChild(table);
+
+        // start timer
+        this.tick();
+
+        // adds the handler
+        addHandler(this.items);
+
+
+        // create the array of pairs of pictures
+        function createArr(img, items) {
+            var i = 0, j = 0, arr = [];
+
+            for (i; i < items / 2; i++) {
+                if (img[j]) {
+                    arr.push(img[j], img[j]);
+                    j++;
+                } else {
+                    j = 0;
+                    arr.push(img[j], img[j]);
+                }
+            }
+            // sorting randomly
+            return arr.sort((a, b) => Math.random() - 0.5);
+        }
+
+
+        // creates the table of pictures
+        function createTable(arr) {
+            var tr, td, widthBox, heightBox, size, image;
+
+            widthBox = this.center.offsetWidth -4 -10; // border and padding
+            heightBox = this.h*0.92-16;
+
+            table = document.createElement('div');
+            table.classList.add('table');
+
+            for (i = 0; i < height; i++) {
+                tr = document.createElement('div');
+                tr.classList.add('tr');
+                for (j = 0; j < width; j++) {
+                    td = document.createElement('div');
+                    size = (widthBox/width < heightBox/height) ? widthBox/width : heightBox/height;
+                    image = arr.pop();
+                    td.style.cssText = 
+                        'width: ' + size + 
+                        'px; height: ' + size + 
+                        'px; background-image: url(' + image + ')';
+                    td.classList.add('td','closed');
+                    td.pair = image;
+                    tr.appendChild(td);
+                }
+                table.appendChild(tr);
+            }
+            return table;
+        }
+
+
+        // adds the handler
+        function addHandler(items) {
+            var duble1, duble2, first, second, third;
+
+            box.addEventListener('click', handler, false);
+            function handler (e) {
+                if (e.target.classList.contains('td')) {
+                    if (e.target === duble1) return;
+                    first = e.target;
+                    second = duble1 ? duble1 : null;
+                    third = duble2 ? duble2 : null;
+                    duble1 = first;
+                    duble2 = second;
+
+                    if (third) {
+                        second.classList.add('closed');
+                        third.classList.add('closed');
+                        duble2 = null;
+                    } else if (second && (first.pair === second.pair)) {
+                        first.classList.add('hide');
+                        second.classList.add('hide');
+                        first.onclick = function(e) {e.stopImmediatePropagation();};
+                        second.onclick = function(e) {e.stopImmediatePropagation();};
+                        duble1 = duble2 = null;
+                        items = items - 2;
+                    }
+                    e.target.classList.remove('closed');
+                }
+                if (items === 0) game.stop();
+            }
+        }
+    };
+
+
+
     Game.prototype.stop = function () {
 
         clearTimeout(this.timerId);
@@ -62,103 +171,29 @@
 
     Game.prototype.tick = function () {
 
-        this.score--;
-        this.timer++;
-        scoreEl.innerHTML = this.score;
-        timerEl.innerHTML = this.timer;
-        this.timerId = setTimeout(this.tick.bind(game), 1000);
-    };
+        var timer, scoreEl, timerEl;
 
-
-
-    Game.prototype.start = function () {
-
-        var box, table, tr, td, widthBox, heightBox, size, duble1, duble2, keys,
-            i = 0, j = 0, arr = [],
-            sumTd = width*height;
-
-        this.score = 1000;
-        this.timer = 0;
-        this.center.innerHTML = "<div class='center-inner'><div class='box' id='box'></div></div>";
-
-        // create the array of pairs of pictures
-        for (i; i < sumTd / 2; i++) {
-            if (this.img[j]) {
-                arr.push(this.img[j], this.img[j]);
-                j++;
-            } else {
-                j = 0;
-                arr.push(this.img[j], this.img[j]);
-            }
-        }
-        // sorting randomly
-        arr.sort((a, b) => Math.random() - 0.5);
-
-        // creates the table
-        box = document.getElementById('box');
-        widthBox = this.center.offsetWidth -4 -10; // border and padding
-        heightBox = this.h*0.92-16;
-
-        table = document.createElement('div');
-        table.classList.add('table');
-
-        for (i = 0; i < height; i++) {
-            tr = document.createElement('div');
-            tr.classList.add('tr');
-            for (j = 0; j < width; j++) {
-                td = document.createElement('div');
-                size = (widthBox/width < heightBox/height) ? widthBox/width : heightBox/height;
-                image = arr.pop();
-                td.style.cssText = 
-                    'width: ' + size + 
-                    'px; height: ' + size + 
-                    'px; background-image: url(' + image + ')';
-                td.classList.add('td','closed');
-                td.pair = image;
-                tr.appendChild(td);
-            }
-            table.appendChild(tr);
-        }
-        box.appendChild(table);
-
-        // start timer
-        this.timerId = setTimeout(this.tick.bind(game), 1000);
         scoreEl = document.getElementById('score');
         timerEl = document.getElementById('timer');
 
-        // adds the handler
-        box.addEventListener('click', handler, false);
-        function handler (e) {
-            if (e.target.classList.contains('td')) {
-                if (e.target === duble1) return;
-                var first = e.target;
-                var second = duble1 ? duble1 : null;
-                var third = duble2 ? duble2 : null;
-                duble1 = first;
-                duble2 = second;
-
-                if (third) {
-                    second.classList.add('closed');
-                    third.classList.add('closed');
-                    duble2 = null;
-                } else if (second && (first.pair === second.pair)) {
-                    first.classList.add('hide');
-                    second.classList.add('hide');
-                    first.onclick = function(e) {e.stopImmediatePropagation();};
-                    second.onclick = function(e) {e.stopImmediatePropagation();};
-                    duble1 = duble2 = null;
-                    sumTd = sumTd-2;
-                }
-                e.target.classList.remove('closed');
-            }
-            if (sumTd === 0) game.stop();
+        function time() {
+            this.score--;
+            this.timer++;
+            scoreEl.innerHTML = this.score;
+            timerEl.innerHTML = this.timer;
+            this.timerId = setTimeout(timer, 1000);
         }
+
+        timer = time.bind(game);
+        this.timerId = setTimeout(timer, 1000);
     };
+
 
 
 var game = new Game();
 // game.ajax();
 game.init();
+
 
 window.clickGame = {};
 window.clickGame.start = function() {game.start();};
